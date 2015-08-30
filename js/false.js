fa62cc3dac6febb03e6d57fc354e40593d92300a
Michael Laszlo <git@michaellaszlo.com>
2015-08-30 00:46:10 -0400

    Or just remove padding from textarea, haha

--- js/false.js
+++ js/false.js
@@ -2,18 +2,5 @@ var False = {
 };
 
 window.onload = function () {
-  var sourceCodeContainer = document.getElementById('sourceCode'),
-      sourceCode = document.getElementById('sourceCode'),
-      codeInput = document.getElementById('codeInput'),
-      gap = codeInput.offsetWidth - sourceCode.clientWidth;
-  console.log(sourceCodeContainer.offsetWidth, sourceCodeContainer.clientWidth);
-  console.log(sourceCode.offsetWidth, sourceCode.clientWidth);
-  console.log(codeInput.offsetWidth, codeInput.clientWidth);
-  console.log(gap);
-  console.log(codeInput.offsetWidth, codeInput.clientWidth);
-  function setWidth() {
-    codeInput.style.width = sourceCode.clientWidth - gap + 'px';
-  }
-  setWidth();
-  window.onresize = setWidth;
+  var codeInput = document.getElementById('codeInput');
 };
